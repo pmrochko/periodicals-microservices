a5807881606bb78a048906bada0b552ef70842f7
Pavlo Mrochko <p.mrochko@gmail.com>
2023-07-10 18:08:01 +0300

    default message edited

--- catalog-service/src/main/java/com/periodicals/catalogservice/model/exception/EntityNotFoundException.java
+++ catalog-service/src/main/java/com/periodicals/catalogservice/model/exception/EntityNotFoundException.java
@@ -5,7 +5,7 @@ package com.periodicals.catalogservice.model.exception;
  */
 public class EntityNotFoundException extends ServiceException {
 
-    private static final String DEFAULT_MESSAGE = "Entity is not found";
+    private static final String DEFAULT_MESSAGE = "Entity not found";
 
     public EntityNotFoundException(){
         super(DEFAULT_MESSAGE);
